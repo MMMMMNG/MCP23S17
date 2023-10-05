e2eeae9e7ab150ca1a493fe696a41b4f32cdaf33
MMMMMNG <113881019+MMMMMNG@users.noreply.github.com>
2023-10-05 09:24:20 +0200

    add test infrastructure

---
+++ src/test/java/com/rrr/mcp23s17/MCP23S17Test.java
@@ -0,0 +1,13 @@
+package com.rrr.mcp23s17;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MCP23S17Test {
+
+    @Test
+    void newWithoutInterrupts() {
+        assertTrue(true);
+    }
+}
