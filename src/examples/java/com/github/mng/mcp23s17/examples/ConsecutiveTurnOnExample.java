739170c5a1c1372c250dba3e43e8741bfeb23eaa
MMMMMNG <113881019+MMMMMNG@users.noreply.github.com>
2023-10-30 17:50:25 +0100

    successfully test examples!
    both examples were successfully tested.
    The provider had to be explicitly requested in the GPIO & SPI ConfigBuilder, then it worked.

--- src/examples/java/com/github/mng/mcp23s17/examples/ConsecutiveTurnOnExample.java
+++ src/examples/java/com/github/mng/mcp23s17/examples/ConsecutiveTurnOnExample.java
@@ -1,22 +1,20 @@
 package com.github.mng.mcp23s17.examples;
 
 import com.pi4j.Pi4J;
-import com.pi4j.io.gpio.digital.DigitalInput;
-import com.pi4j.io.gpio.digital.DigitalInputConfigBuilder;
 import com.pi4j.io.spi.SpiBus;
 import com.pi4j.io.spi.SpiChipSelect;
 import com.pi4j.io.spi.SpiConfigBuilder;
-import com.pi4j.util.Console;
+import com.pi4j.plugin.pigpio.provider.spi.PiGpioSpiProvider;
 import com.rrr.mcp23s17.MCP23S17;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
 public class ConsecutiveTurnOnExample {
     private static final Logger log = LoggerFactory.getLogger(ConsecutiveTurnOnExample.class);
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) {
         log.info("'sup, starting MCP23S17 turn-on example.");
         log.info("whoohoo 🔥");
 
@@ -25,37 +23,59 @@ public class ConsecutiveTurnOnExample {
         var spi = context.create(SpiConfigBuilder.newInstance(context)
                 .id("MCP23S17 example")
                 .bus(SpiBus.BUS_0)
-                .baud(10_000_000)
+                .baud(1_000_000)
                 .channel(0)
                 .chipSelect(SpiChipSelect.CS_0)
+                .provider(PiGpioSpiProvider.class)
                 .build()
         );
 
-        var intChip0 = context.create(DigitalInputConfigBuilder.newInstance(context)
-                .id("MCP23S17 chip0 irupt")
-                .address(22)
-                .build()
-        );
-
-        var intChip1 = context.create(DigitalInputConfigBuilder.newInstance(context)
-                .id("MCP23S17 chip1 irupt")
-                .address(23)
-                .build()
-        );
-
-        var chips = MCP23S17.multipleNewOnSameBusWithTiedInterrupts(spi,new DigitalInput[]{intChip0, intChip1},2,true);
+        var chips = MCP23S17.multipleNewOnSameBus(spi,2);
 
         var pins = new ArrayList<MCP23S17.PinView>(32);
-        pins.addAll(chips.get(0).getAllPinsAsPulledUpInterruptInput());
-        pins.addAll(chips.get(1).getAllPinsAsPulledUpInterruptInput());
 
-        for (int i = 0; i < pins.size(); i++) {
-            int finalI = i;
-            pins.get(i).addListener((m, p) -> log.info("detected statechange to {} on pin {}", m, finalI));
+        //init all pins to outputs
+        for(var chip : chips){
+            var iter = chip.getPinViewIterator();
+            while(iter.hasNext()){
+                var pin = iter.next();
+                pin.setAsOutput();
+                pins.add(pin);
+            }
+            chip.writeIODIRA();
+            chip.writeIODIRB();
+        }
+        try {
+            mainApp(chips, pins);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        } finally {
+            context.shutdown();
         }
 
-        var cons = new Console();
-        cons.box("wait for exit");
-        cons.waitForExit();
+    }
+
+    private static void mainApp(List<MCP23S17> chips, ArrayList<MCP23S17.PinView> pins) throws InterruptedException {
+        int oldIndex =  0;
+        int onIndex = 0;
+        //start actual app
+        log.info("starting example...");
+        while(!Thread.interrupted()){
+            if(onIndex == 0 && oldIndex == 31){
+                log.info("Went through all 32 pins.");
+            }
+            //change state
+            pins.get(oldIndex).clear();
+            pins.get(onIndex).set();
+            //write to every possible output register
+            chips.get(0).writeOLATA();
+            chips.get(0).writeOLATB();
+            chips.get(1).writeOLATA();
+            chips.get(1).writeOLATB();
+            //increase index
+            oldIndex = onIndex;
+            onIndex = (onIndex + 1) % pins.size();
+            Thread.sleep(100);
+        }
     }
 }
--- src/examples/java/com/github/mng/mcp23s17/examples/InputInterruptExample.java
+++ src/examples/java/com/github/mng/mcp23s17/examples/InputInterruptExample.java
@@ -6,6 +6,8 @@ import com.pi4j.io.gpio.digital.DigitalInputConfigBuilder;
 import com.pi4j.io.spi.SpiBus;
 import com.pi4j.io.spi.SpiChipSelect;
 import com.pi4j.io.spi.SpiConfigBuilder;
+import com.pi4j.plugin.pigpio.provider.gpio.digital.PiGpioDigitalInputProvider;
+import com.pi4j.plugin.pigpio.provider.spi.PiGpioSpiProvider;
 import com.pi4j.util.Console;
 import com.rrr.mcp23s17.MCP23S17;
 import org.slf4j.Logger;
@@ -16,7 +18,7 @@ import java.util.ArrayList;
 
 public class InputInterruptExample {
     private static final Logger log = LoggerFactory.getLogger(InputInterruptExample.class);
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws IOException {
         log.info("'sup, starting MCP23S17 interrupt example.");
         log.info("whoohoo 🔥");
 
@@ -25,21 +27,24 @@ public class InputInterruptExample {
         var spi = context.create(SpiConfigBuilder.newInstance(context)
                 .id("MCP23S17 example")
                 .bus(SpiBus.BUS_0)
-                .baud(10_000_000)
+                .baud(1_000_000)
                 .channel(0)
                 .chipSelect(SpiChipSelect.CS_0)
+                .provider(PiGpioSpiProvider.class)
                 .build()
         );
 
         var intChip0 = context.create(DigitalInputConfigBuilder.newInstance(context)
                 .id("MCP23S17 chip0 irupt")
                 .address(22)
+                .provider(PiGpioDigitalInputProvider.class)
                 .build()
         );
 
         var intChip1 = context.create(DigitalInputConfigBuilder.newInstance(context)
                 .id("MCP23S17 chip1 irupt")
                 .address(23)
+                .provider(PiGpioDigitalInputProvider.class)
                 .build()
         );
 
@@ -56,6 +61,12 @@ public class InputInterruptExample {
 
         var cons = new Console();
         cons.box("wait for exit");
-        cons.waitForExit();
+        try {
+            cons.waitForExit();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        } finally {
+            context.shutdown();
+        }
     }
 }
